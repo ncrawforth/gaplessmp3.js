8ea09dd9e9b8a3d5ff2d13d8f7152462577f72d5
Neil Crawforth <16989539+ncrawforth@users.noreply.github.com>
2024-01-19 16:44:47 +0000

    Implement missing functions

--- gaplessmp3.js
+++ gaplessmp3.js
@@ -5,10 +5,12 @@ class GaplessMP3 {
     const bufferChunks = 10;
     let tracks = [];
     let currentTrack = 0;
+    let currentTime = 0;
+    let trackTimestamps = [0];
     async function mediaSourceOpen(e) {
       e.target.onsourceopen = null;
       let buffer = [];
-      let bufferTrack = 0;
+      let bufferTrack = currentTrack;
       let bufferByte = 0;
       let sourceBuffer = e.target.addSourceBuffer("audio/mpeg");
       let closed = false;
@@ -29,27 +31,37 @@ class GaplessMP3 {
         }
         if (buffer.length > 0) {
           try {
-            if (bufferTrack == tracks.length && buffer[0].byteLength < chunkSize) {
-              sourceBuffer.onupdateend = function() {
-                sourceBuffer.onupdateend = null;
-                e.target.endOfStream();
-              };
-            } else {
-              sourceBuffer.onupdateend = function() {
-                sourceBuffer.onupdateend = null;
-                e.target.duration = sourceBuffer.timestampOffset;
-              };
-            }
             sourceBuffer.appendBuffer(buffer[0]);
+            await new Promise(function(resolve) {sourceBuffer.onupdateend = resolve;});
+            sourceBuffer.onupdateend = null;
+            if (buffer[0].byteLength < chunkSize) {
+              if (bufferTrack == tracks.length) {
+                e.target.endOfStream();
+              }
+              trackTimestamps[bufferTrack] = sourceBuffer.timestampOffset;
+            }
             buffer.shift();
           } catch {}
         }
         await new Promise(function(resolve) {setTimeout(resolve, 2000);});
       }
     };
-    Object.defineProperty(player, "currentTrack", {get: function() {return currentTrack;}});
+    Object.defineProperty(player, "currentTrack", {get: function() {
+      return currentTrack;
+    }, set: function(v) {
+      trackTimestamps = [0];
+      currentTrack = Math.max(0, Math.min(v, tracks.length - 1));
+      currentTime = 0;
+      let mediaSource = new MediaSource();
+      mediaSource.onsourceopen = mediaSourceOpen;
+      mediaElement.src = URL.createObjectURL(mediaSource);
+    }});
+    Object.defineProperty(player, "currentTime", {get: function() {
+      return currentTime;
+    }});
     Object.defineProperty(player, "onplay", {writable: true});
     Object.defineProperty(player, "onplaying", {writable: true});
+    Object.defineProperty(player, "onwaiting", {writable: true});
     Object.defineProperty(player, "onpause", {writable: true});
     Object.defineProperty(player, "ontimeupdate", {writable: true});
     player.addTrack = function(url) {
@@ -58,7 +70,9 @@ class GaplessMP3 {
     };
     player.clearTracks = function() {
       tracks = [];
+      trackTimestamps = [0];
       currentTrack = 0;
+      currentTime = 0;
       let mediaSource = new MediaSource();
       mediaSource.onsourceopen = mediaSourceOpen;
       mediaElement.src = URL.createObjectURL(mediaSource);
@@ -67,7 +81,17 @@ class GaplessMP3 {
     player.pause = function() {mediaElement.pause();};
     mediaElement.addEventListener("play", function() {if (player.onplay) player.onplay();});
     mediaElement.addEventListener("playing", function() {if (player.onplaying) player.onplaying();});
+    mediaElement.addEventListener("waiting", function() {if (player.onplaying) player.onwaiting();});
     mediaElement.addEventListener("pause", function() {if (player.onpause) player.onpause();});
+    mediaElement.addEventListener("timeupdate", function() {
+      for (let i = 0; i < tracks.length; i++) {
+        if (trackTimestamps[i] != undefined && mediaElement.currentTime >= trackTimestamps[i]) {
+          currentTrack = i;
+        }
+      }
+      currentTime = mediaElement.currentTime - trackTimestamps[currentTrack];
+      if (player.ontimeupdate) player.ontimeupdate();
+    });
     player.clearTracks();
   }
 }
