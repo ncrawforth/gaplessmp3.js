45721163006d6ad142778cf5e97ada4cc526d2d2
Neil Crawforth <16989539+ncrawforth@users.noreply.github.com>
2024-01-19 12:35:14 +0000

    Add files via upload

--- gaplessmp3.js
+++ gaplessmp3.js
@@ -1,26 +1,31 @@
 class GaplessMP3 {
   constructor(mediaElement) {
+    let player = this;
     const chunkSize = 524288;
     const bufferChunks = 10;
     let tracks = [];
     let currentTrack = 0;
-    Object.defineProperty(this, "currentTrack", {get: function() {return currentTrack;}});
-    Object.defineProperty(this, "onplay", {writable: true});
-    Object.defineProperty(this, "onpause", {writable: true});
-    Object.defineProperty(this, "ontimeupdate", {writable: true});
+    let mediaSource = new MediaSource();
+    Object.defineProperty(player, "currentTrack", {get: function() {return currentTrack;}});
+    Object.defineProperty(player, "onplay", {writable: true});
+    Object.defineProperty(player, "onplaying", {writable: true});
+    Object.defineProperty(player, "onpause", {writable: true});
+    Object.defineProperty(player, "ontimeupdate", {writable: true});
     this.addTrack = function(url) {
       tracks.push(url);
       return tracks.length - 1;
     };
-    let mediaSource = new MediaSource();
+    player.play = function() {mediaElement.play();};
+    player.pause = function() {mediaElement.pause();};
+    mediaElement.addEventListener("play", function() {if (player.onplay) player.onplay();});
+    mediaElement.addEventListener("playing", function() {if (player.onplaying) player.onplaying();});
+    mediaElement.addEventListener("pause", function() {if (player.onpause) player.onpause();});
     mediaSource.onsourceopen = async function() {
       mediaSource.onsourceopen = null;
       let buffer = [];
       let bufferTrack = 0;
       let bufferByte = 0;
       let sourceBuffer = mediaSource.addSourceBuffer("audio/mpeg");
-      window.sourceBuffer = sourceBuffer;
-      window.mediaSource = mediaSource;
       while (true) {
         if (buffer.length < bufferChunks && bufferTrack < tracks.length) {
           try {
@@ -38,16 +43,23 @@ class GaplessMP3 {
             }
           } catch {}
         }
-        try {
-          if (bufferTrack == tracks.length && buffer[0].byteLength < chunkSize) {
-            sourceBuffer.onupdateend = function() {
-              sourceBuffer.onupdateend = null;
-              mediaSource.endOfStream();
-            };
-          }
-          sourceBuffer.appendBuffer(buffer[0]);
-          buffer.shift();
-        } catch {}
+        if (buffer.length > 0) {
+          try {
+            if (bufferTrack == tracks.length && buffer[0].byteLength < chunkSize) {
+              sourceBuffer.onupdateend = function() {
+                sourceBuffer.onupdateend = null;
+                mediaSource.endOfStream();
+              };
+            } else {
+              sourceBuffer.onupdateend = function() {
+                sourceBuffer.onupdateend = null;
+                mediaSource.duration = sourceBuffer.timestampOffset;
+              };
+            }
+            sourceBuffer.appendBuffer(buffer[0]);
+            buffer.shift();
+          } catch {}
+        }
         await new Promise(function(resolve) {setTimeout(resolve, 2000);});
       }
     };
